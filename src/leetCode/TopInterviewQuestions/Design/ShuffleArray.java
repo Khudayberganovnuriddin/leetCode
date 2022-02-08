d0e7fd0d1371378fd9411a9c84574f34269836f2
Nuriddin Khudayberganov <khudayberganovnuriddin@gmail.com>
2022-02-08 17:06:46 +0500

    Design done ------------------------ 1. Shuffle Array 2. Min Stack

---
+++ src/leetCode/TopInterviewQuestions/Design/ShuffleArray.java
@@ -0,0 +1,31 @@
+package leetCode.TopInterviewQuestions.Design;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ShuffleArray {
+
+    private final int[] nums;
+
+    public ShuffleArray(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int[] reset() {
+        return this.nums;
+    }
+
+    public int[] shuffle() {
+        int[] shuffleArr = new int[nums.length];
+        shuffleArr = Arrays.copyOf(nums,nums.length);
+        Random random = ThreadLocalRandom.current();
+        for (int i = nums.length - 1; i > 0; i--) {
+            int ind = random.nextInt(i + 1);
+            int temp = shuffleArr[ind];
+            shuffleArr[ind] = shuffleArr[i];
+            shuffleArr[i] = temp;
+        }
+        return shuffleArr;
+    }
+}
