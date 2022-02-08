e29c56d05da77b298d323caf04d168ca27b46bbf
Nuriddin Khudayberganov <khudayberganovnuriddin@gmail.com>
2022-02-08 15:38:44 +0500

    Climbing Stairs done

---
+++ src/leetCode/TopInterviewQuestions/DynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,20 @@
+package leetCode.TopInterviewQuestions.DynamicProgramming;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int num1 = 1;
+        int num2 = 2;
+        int resp = 0;
+
+        for (int i = 2; i < n; i++) {
+            resp = num1 + num2;
+            num1 = num2;
+            num2 = resp;
+        }
+
+        return resp;
+    }
+}
