38d564392f2c7045b9520474b937175c0c2faf92
Nuriddin Khudayberganov <khudayberganovnuriddin@gmail.com>
2022-05-07 12:20:12 +0500

    Wix Engineering added

---
+++ src/leetCode/wix_engineering/SolutionWix.java
@@ -0,0 +1,8 @@
+package leetCode.wix_engineering;
+
+public class SolutionWix {
+    public static void main(String[] args) {
+        String[] arr = new String[]{"b@leetcode.com", "a@leetcode.com", "c@leetcode.com"};
+        System.out.println(UniqueEmailAdresses.numUniqueEmails(arr));
+    }
+}
