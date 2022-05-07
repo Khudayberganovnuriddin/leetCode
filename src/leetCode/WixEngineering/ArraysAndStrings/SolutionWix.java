19fe89b248cf937e0d216da258a5004faad668a7
Nuriddin Khudayberganov <khudayberganovnuriddin@gmail.com>
2022-05-07 12:51:44 +0500

    VerifyingAnAlienDictionary

--- src/leetCode/WixEngineering/ArraysAndStrings/SolutionWix.java
+++ src/leetCode/WixEngineering/ArraysAndStrings/SolutionWix.java
@@ -2,7 +2,10 @@ package leetCode.WixEngineering.ArraysAndStrings;
 
 public class SolutionWix {
     public static void main(String[] args) {
-        String[] arr = new String[]{"b@leetcode.com", "a@leetcode.com", "c@leetcode.com"};
-        System.out.println(UniqueEmailAdresses.numUniqueEmails(arr));
+//        String[] arr = new String[]{"b@leetcode.com", "a@leetcode.com", "c@leetcode.com"};
+//        System.out.println(UniqueEmailAdresses.numUniqueEmails(arr));
+
+        String[] arr = new String[]{"hello","leetcode"};
+        System.out.println(VerifyingAnAlienDictionary.isAlienSorted(arr,"hlabcdefgijkmnopqrstuvwxyz"));
     }
 }
