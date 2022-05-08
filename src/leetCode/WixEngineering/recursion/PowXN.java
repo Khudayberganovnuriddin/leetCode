10be82beacf2db972d80de72a33118415ac8e422
Khudayberganovnuriddin <Khudayberganovnuriddin@gmail.com>
2022-05-08 12:48:39 +0500

    Wix Engineer Recursion DONE

---
+++ src/leetCode/WixEngineering/recursion/PowXN.java
@@ -0,0 +1,9 @@
+package leetCode.WixEngineering.recursion;
+
+public class PowXN {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n < 0) return 1 / x * myPow(1 / x, -(n + 1));
+        return (n & 1) == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
